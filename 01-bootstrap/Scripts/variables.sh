d1fc7890e1118576ffb7c412eb6e1f1a175258ed
senthil <senthil.veeranan@gmail.com>
2026-07-14 03:29:37 +0530

    Update variables.sh

--- 01-bootstrap/Scripts/variables.sh
+++ 01-bootstrap/Scripts/variables.sh
@@ -1,17 +1,135 @@
 #!/bin/bash
+#===============================================================================
+# Ultimate DevOps Lab
+# File    : variables.sh
+# Purpose : Global Variables
+# OS      : Rocky Linux 9.x
+# Author  : Senthil Kumar
+#===============================================================================
 
-LAB_DOMAIN="lab.local"
+#------------------------------------------
+# Lab Information
+#------------------------------------------
+export LAB_NAME="Ultimate DevOps Lab"
+export LAB_VERSION="1.0"
+export LAB_DOMAIN="lab.local"
 
-TIMEZONE="Asia/Kolkata"
+#------------------------------------------
+# Timezone
+#------------------------------------------
+export TIMEZONE="Asia/Kolkata"
 
-DEVOPS_USER="devops"
+#------------------------------------------
+# Default User
+#------------------------------------------
+export DEVOPS_USER="devops"
+export DEVOPS_GROUP="devops"
 
-SSH_PORT=22
+#------------------------------------------
+# SSH
+#------------------------------------------
+export SSH_PORT="22"
 
-NODES=(
-ansible
-docker
-web1
-web2
-utility
+#------------------------------------------
+# Network
+#------------------------------------------
+export NETWORK_PREFIX="192.168.56"
+
+#------------------------------------------
+# Hostnames
+#------------------------------------------
+export ANSIBLE_HOST="ansible"
+export DOCKER_HOST="docker"
+export WEB1_HOST="web1"
+export WEB2_HOST="web2"
+export UTILITY_HOST="utility"
+
+#------------------------------------------
+# IP Addresses
+#------------------------------------------
+export ANSIBLE_IP="${NETWORK_PREFIX}.10"
+export DOCKER_IP="${NETWORK_PREFIX}.20"
+export WEB1_IP="${NETWORK_PREFIX}.30"
+export WEB2_IP="${NETWORK_PREFIX}.40"
+export UTILITY_IP="${NETWORK_PREFIX}.50"
+
+#------------------------------------------
+# Common Packages
+#------------------------------------------
+COMMON_PACKAGES=(
+git
+curl
+wget
+vim
+nano
+tree
+jq
+tar
+zip
+unzip
+rsync
+bind-utils
+net-tools
+bash-completion
+python3
+python3-pip
+openssh-clients
+chrony
+firewalld
+policycoreutils-python-utils
 )
+
+#------------------------------------------
+# Services
+#------------------------------------------
+SERVICES=(
+sshd
+chronyd
+firewalld
+)
+
+#------------------------------------------
+# Directories
+#------------------------------------------
+export LOG_DIR="/var/log/devops-lab"
+export SCRIPT_DIR="/opt/devops-lab"
+export BACKUP_DIR="/backup"
+
+#------------------------------------------
+# Inventory
+#------------------------------------------
+export INVENTORY_FILE="/vagrant/inventory/inventory.ini"
+
+#------------------------------------------
+# Colors
+#------------------------------------------
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[0;34m"
+CYAN="\033[0;36m"
+WHITE="\033[1;37m"
+NC="\033[0m"
+
+#------------------------------------------
+# Log File
+#------------------------------------------
+export LOG_FILE="${LOG_DIR}/bootstrap.log"
+
+#------------------------------------------
+# Kubernetes
+#------------------------------------------
+export KUBERNETES_VERSION="v1.33"
+export HELM_VERSION="v3"
+
+#------------------------------------------
+# Docker
+#------------------------------------------
+export DOCKER_VERSION="latest"
+
+#------------------------------------------
+# Script Information
+#------------------------------------------
+SCRIPT_NAME=$(basename "$0")
+DATE=$(date '+%F')
+TIME=$(date '+%T')
