eb7069cb5ed16adbc8ad0036dbdc447cd2567480
senthil <senthil.veeranan@gmail.com>
2026-07-14 03:25:44 +0530

    Update common.sh

--- 01-bootstrap/Scripts/common.sh
+++ 01-bootstrap/Scripts/common.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 
+source logger.sh
+source variables.sh
+source functions.sh
+
+info "Updating System"
+
+dnf update -y
+
+info "Installing Common Packages"
+
+install_package git
+
+install_package curl
+
+install_package wget
+
+install_package vim
+
+install_package tree
+
+install_package jq
+
+install_package python3
+
+success "Bootstrap Completed"
